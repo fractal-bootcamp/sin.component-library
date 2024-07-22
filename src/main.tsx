8ea8abd7bd480d87640d47fc4ee57acfa9ad88a4
Iyana <114933494+yayakixtenet@users.noreply.github.com>
2024-07-22 16:28:29 -0400

    add checkboxes

--- src/main.tsx
+++ src/main.tsx
@@ -2,19 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import Dropdown from './components/dropdown/Dropdown.tsx'
-const exampleProps = {
-  options: [
-    { label: 'Default Option 1', value: 'default1' },
-    { label: 'Default Option 2', value: 'default2' }
-  ],
-  defaultText: 'Select an option',
-  disabled: false,
-}
-
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Dropdown props={exampleProps} />
+    <App />
   </React.StrictMode>,
 )
