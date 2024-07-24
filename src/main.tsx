3070ccfd0714dae4a9b0687486a9076e7bd75ade
Iyana <114933494+yayakixtenet@users.noreply.github.com>
2024-07-24 14:24:20 -0400

    restore main

--- src/main.tsx
+++ src/main.tsx
@@ -2,20 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import Rating, { RatingProps } from './components/Rating/Rating.tsx'
-
-const props: RatingProps = {
-  totalStars: 6,
-  shape: '\u2665', // Optional prop
-  disabled: false, // Optional prop,
-  readOnly: true,
-  readOnlyValue: 3
-};
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  // <React.StrictMode>
-  // {/* </React.StrictMode>, */ }
-  <>
-    <Rating {...props} />
-  </>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+
 )
